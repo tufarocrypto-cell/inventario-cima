2f9352e08c3a4fcb9a163bd7283aae08764e3385
tufarocrypto-cell <tufarocrypto@gmail.com>
2026-09-08 14:36:40 +0200

    Pubblica v0.1.10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSIONE = "0.1.9";
+const VERSIONE = "0.1.10";
 const CACHE = "inventario-" + VERSIONE;
 const FILE = ["./", "./index.html", "./manifest.webmanifest", "./icona.svg"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILE))); });
